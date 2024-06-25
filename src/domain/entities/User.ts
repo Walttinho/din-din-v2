57431fc3ee6aab0504c1df3c4691de3a89dbe314
walttinho <walter.netto@live.com>
2024-06-25 16:11:33 -0300

    feat(user): add User entity - Added User entity to represent user domain model.

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,48 @@
+import { Replace } from '../../utils/replace';
+import { IUser } from '../interfaces/IUser';
+import { randomUUID } from 'crypto';
+
+export class User {
+  private props: IUser;
+  private _id: string;
+
+  constructor(props: Replace<IUser, { createdAt?: Date }>, id?: string) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt || new Date(),
+    };
+    this._id = id || randomUUID();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  set email(email: string) {
+    this.props.email = email;
+  }
+
+  set password(password: string) {
+    this.props.password = password;
+  }
+}
