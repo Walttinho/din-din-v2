2cd89fdd83f5752b46a65937cfb5c1b8102cfd3c
walttinho <walter.netto@live.com>
2024-06-25 16:20:29 -0300

    feat(app): update app module to include database and user modules - Updated app.module.ts to include DatabaseModule and UserModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { DatabaseModule } from './module/database.module';
+import { UserModule } from './module/user.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [],
   providers: [],
 })
