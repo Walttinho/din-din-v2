04c083f125e9ec6883c1c5bdc6c759cbb0291e2f
walttinho <walter.netto@live.com>
2024-06-26 03:18:31 -0300

    feat(user.module): register GetUserByEmailController and use case - Registered GetUserByEmailController and GetUserByEmailUseCase in user.module.ts.

--- src/module/user.module.ts
+++ src/module/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { CreateUserController } from 'src/application/user/controller/CreateUserController/CreateUserController';
 import { CreateUserUseCase } from 'src/application/user/usecase/CreateUserUseCase/CreateUserUseCase';
 import { DatabaseModule } from './database.module';
+import { GetUserByEmailController } from 'src/application/user/controller/GetUserByEmailController/GetUserByEmailController';
+import { GetUserByEmailUseCase } from 'src/application/user/usecase/GetUserByEmailUseCase/GetUserByEmailUseCase';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController],
-  providers: [CreateUserUseCase],
+  controllers: [CreateUserController, GetUserByEmailController],
+  providers: [CreateUserUseCase, GetUserByEmailUseCase],
 })
 export class UserModule {}
