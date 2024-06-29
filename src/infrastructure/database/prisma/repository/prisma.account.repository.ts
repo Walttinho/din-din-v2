e343997bdfec754ba384b67a6fc764f7d41a3c75
Walttinho <walter.netto@live.com>
2024-06-29 14:21:39 -0300

    feat(prisma): add PrismaAccountRepository - Added PrismaAccountRepository to handle account persistence using Prisma.

---
+++ src/infrastructure/database/prisma/repository/prisma.account.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { AccountRepository } from '../../../../domain/repositories/account.repository';
+import { PrismaService } from '../prisma.service';
+import { Account } from '../../../../domain/entities/Account';
+import { PrismaAccountMapper } from '../mappers/prisma.account.mapper';
+import { PrismaPaymentMapper } from '../mappers/prisma.payment.mapper';
+
+@Injectable()
+export class PrismaAccountRepository extends AccountRepository {
+  constructor(private prisma: PrismaService) {
+    super();
+  }
+
+  async create(account: Account): Promise<any> {
+    const accountRaw = PrismaAccountMapper.toPrisma(account);
+    const result = await this.prisma.account.create({
+      data: accountRaw,
+    });
+
+    return result;
+  }
+
+  async findById(id: string, startDate: Date, endDate: Date): Promise<Account> {
+    const accountRaw = await this.prisma.account.findUnique({
+      where: { id },
+      include: {
+        payments: {
+          where: {
+            createdAt: {
+              gte: startDate,
+              lte: endDate,
+            },
+          },
+        },
+      },
+    });
+
+    const payments = accountRaw.payments.map(PrismaPaymentMapper.toDomain);
+    const account = PrismaAccountMapper.toDomain(accountRaw);
+    account.payments = payments;
+    return account;
+  }
+
+  async update(account: Account): Promise<Account> {
+    const accountRaw = PrismaAccountMapper.toPrisma(account);
+    await this.prisma.account.update({
+      where: { id: accountRaw.id },
+      data: accountRaw,
+    });
+    return;
+  }
+}
