a1a1fada355665e98ad79312ad7ace166300556a
Walttinho <walter.netto@live.com>
2024-06-29 14:21:39 -0300

    feat(app): import Account and Payment modules - Updated app.module.ts to import AccountModule and PaymentModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './module/database.module';
 import { UserModule } from './module/user.module';
+import { AccountModule } from './module/account.module';
+import { PaymentModule } from './module/payment.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, AccountModule, PaymentModule],
   controllers: [],
   providers: [],
 })
