84f782bce5005322782b12ada0050063eacd4e0a
Walttinho <walter.netto@live.com>
2024-06-29 14:21:39 -0300

    test(CreateAccountUseCase): add unit tests - Added unit tests for CreateAccountUseCase to cover various scenarios including successful account creation and validation errors.

---
+++ src/application/account/usecase/CreateAccountUseCase/CreateAccountUseCase.spec.ts
@@ -0,0 +1,94 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateAccountUseCase } from './CreateAccountUseCase';
+import { PrismaAccountRepository } from '../../../../infrastructure/database/prisma/repository/prisma.account.repository';
+import { Account } from '../../../../domain/entities/Account';
+import { BadRequestException } from '@nestjs/common';
+
+describe('CreateAccountUseCase', () => {
+  let useCase: CreateAccountUseCase;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreateAccountUseCase,
+        {
+          provide: PrismaAccountRepository,
+          useValue: {
+            create: jest.fn().mockImplementation((account) => {
+              return Promise.resolve(
+                new Account({
+                  ...account.props,
+                  _id: account._id,
+                }),
+              );
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    useCase = module.get<CreateAccountUseCase>(CreateAccountUseCase);
+  });
+
+  it('should be defined', () => {
+    expect(useCase).toBeDefined();
+  });
+
+  it('should create a new account with success', async () => {
+    const result = await useCase.execute({
+      name: 'Walter Netto',
+      type: 'CURRENT',
+      balance: 1000,
+    });
+
+    expect(result).toHaveProperty('id');
+    expect(result).toHaveProperty('name', 'Walter Netto');
+    expect(result).toHaveProperty('type', 'CURRENT');
+    expect(result).toHaveProperty('balance', 1000);
+  });
+
+  it('should throw BadRequestException if balance is negative', async () => {
+    try {
+      await useCase.execute({
+        name: 'Walter Netto',
+        type: 'CURRENT',
+        balance: -1000,
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+      expect(error.message).toEqual('Initial balance cannot be negative');
+      expect(error.error).toEqual('BadRequestException');
+      expect(error.statusCode).toEqual(400);
+    }
+  });
+
+  it('should throw BadRequestException if name is missing', async () => {
+    try {
+      await useCase.execute({
+        name: '',
+        type: 'CURRENT',
+        balance: 1000,
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+      expect(error.message).toEqual('name should not be empty');
+      expect(error.error).toEqual('BadRequestException');
+      expect(error.statusCode).toEqual(400);
+    }
+  });
+
+  it('should throw BadRequestException if type is invalid', async () => {
+    try {
+      await useCase.execute({
+        name: 'Walter Netto',
+        type: 'INVALID' as any,
+        balance: 1000,
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+      expect(error.message).toEqual('Invalid account type');
+      expect(error.error).toEqual('BadRequestException');
+      expect(error.statusCode).toEqual(400);
+    }
+  });
+});
