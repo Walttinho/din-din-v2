69619e107098c2b68f9a7782f8b7867110691b9f
walttinho <walter.netto@live.com>
2024-06-30 17:35:56 -0300

    feat(account): protect GetAccountByIdController with AuthGuard - Added AuthGuard to GetAccountByIdController to protect the get account by ID endpoint.

--- src/application/account/controller/GetAccountByIdController/GetAccountByIdController.ts
+++ src/application/account/controller/GetAccountByIdController/GetAccountByIdController.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { GetAccountByIdUseCase } from '../../usecase/GetAccountByIdUseCase/GetAccountByIdUseCase';
 import { viewModelAccount } from '../../viewModels/account.viewModel';
-
+import { AuthGuard } from '../../../../guards/auth.guard';
 
 @Controller('account')
 export class GetAccountByIdController {
   constructor(private readonly getAccountByIdUseCase: GetAccountByIdUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async execute(
     @Query('id') id: string,
