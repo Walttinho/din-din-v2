7d6c6aa99762ea149f8067cb4cb9054359972368
walttinho <walter.netto@live.com>
2024-06-30 17:35:56 -0300

    feat(account): protect CreateAccountController with AuthGuard - Added AuthGuard to CreateAccountController to protect the create account endpoint.

--- src/application/account/controller/CreateAccountController/CreateAccountController.ts
+++ src/application/account/controller/CreateAccountController/CreateAccountController.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateAccountUseCase } from '../../usecase/CreateAccountUseCase/CreateAccountUseCase';
 import { CreateAccountDto } from '../../../../domain/dtos/CreateAccountDto';
+import { AuthGuard } from '../../../../guards/auth.guard';
 
 @Controller('account')
 export class CreateAccountController {
   constructor(private readonly createAccountUseCase: CreateAccountUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async execute(@Body() createAccountDto: CreateAccountDto) {
     const result = await this.createAccountUseCase.execute(createAccountDto);
