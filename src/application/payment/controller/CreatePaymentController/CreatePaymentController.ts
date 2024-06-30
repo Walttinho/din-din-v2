39a0e6541ca67b7a820182cbae5196ed0bd26580
walttinho <walter.netto@live.com>
2024-06-30 17:35:56 -0300

    feat(payment): protect CreatePaymentController with AuthGuard - Added AuthGuard to CreatePaymentController to protect the create payment endpoint.

--- src/application/payment/controller/CreatePaymentController/CreatePaymentController.ts
+++ src/application/payment/controller/CreatePaymentController/CreatePaymentController.ts
@@ -1,12 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { Post, Body } from '@nestjs/common';
 import { CreatePaymentDto } from '../../../../domain/dtos/CreatePaymentDto';
 import { CreatePaymentUseCase } from '../../useCase/CreatePaymentUseCase/CreatePaymentUseCase';
+import { AuthGuard } from '../../../../guards/auth.guard';
 
 @Controller('payment')
 export class CreatePaymentController {
   constructor(private readonly createPaymentUseCase: CreatePaymentUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async execute(@Body() createPaymentDto: CreatePaymentDto) {
     const result = await this.createPaymentUseCase.execute(createPaymentDto);
