d71563972193c72cf9ec15eed8aca99a840b0532
walttinho <walter.netto@live.com>
2024-06-30 17:35:56 -0300

    feat(user): protect GetUserByEmailController with AuthGuard - Added AuthGuard to GetUserByEmailController to protect the get user by email endpoint.

--- src/application/user/controller/GetUserByEmailController/GetUserByEmailController.ts
+++ src/application/user/controller/GetUserByEmailController/GetUserByEmailController.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { GetUserByEmailUseCase } from '../../usecase/GetUserByEmailUseCase/GetUserByEmailUseCase';
+import { AuthGuard } from '../../../../guards/auth.guard';
 
 @Controller('user')
 export class GetUserByEmailController {
   constructor(private readonly getUserByEmailUseCase: GetUserByEmailUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async execute(@Query('email') email: string) {
     const user = await this.getUserByEmailUseCase.execute(email);
