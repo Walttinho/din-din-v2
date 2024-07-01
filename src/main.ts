a342acb4906cfcbd16d3fdaf542eb729aeb7a795
Walttinho <walter.netto@live.com>
2024-07-01 05:44:47 -0300

    feat(swagger): add Swagger documentation - Integrated Swagger documentation in main.ts to document DIN-DIN.v2 API endpoints.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ErrorFilter } from './filters/error.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ErrorFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle(
+      'DIN-DIN.v2 - A RESTful API for Managing Bank Accounts and Payments',
+    )
+    .setDescription(
+      'The API, developed with NestJS, aims to provide an efficient system for managing bank accounts and payments. It leverages the Prisma ORM for seamless database interactions, Docker for hassle-free development and deployment, and GitHub Actions for continuous integration and delivery. The project adheres to best practices such as Domain-Driven Design (DDD) and Test-Driven Development (TDD), ensuring a robust and scalable application. Key features include user authentication, account creation and management, and payment transactions. It supports various account types and allows for detailed transaction filtering. Designed for scalability, it offers a comprehensive solution for financial management with a focus on usability and security.',
+    )
+    .setVersion('1.0')
+    .setContact(
+      'Walter Netto',
+      'https://github.com/walttinho',
+      'walter.netto@live.com',
+    )
+    .addBearerAuth()
+    .addTag('User')
+    .addTag('Auth')
+    .addTag('Account')
+    .addTag('Payment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(3000);
 }
