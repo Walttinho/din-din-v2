1a320b922ae1aae5c269389402c0add31caa2031
Walttinho <walter.netto@live.com>
2024-07-01 05:44:47 -0300

    feat(dto): add CreatePaymentDto - Created CreatePaymentDto to specify the data required for initiating payment transactions.

--- src/domain/dtos/CreatePaymentDto.ts
+++ src/domain/dtos/CreatePaymentDto.ts
@@ -1,16 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
 
 export class CreatePaymentDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: '12018f9b-4859-4e21-a0f1-45f990fa99c7',
+    description: 'The ID of the account',
+  })
   accountId: string;
 
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
+  @ApiProperty({ example: 214, description: 'The amount of the payment' })
   amount: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'some description',
+    description: 'The description of the payment',
+  })
   description: string;
 }
