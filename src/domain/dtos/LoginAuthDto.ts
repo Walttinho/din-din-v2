91788ca5f3f1b1f20fc637c4c6c07452b924fc1d
Walttinho <walter.netto@live.com>
2024-07-01 05:44:47 -0300

    feat(dto): add LoginAuthDto - Implemented LoginAuthDto to handle user authentication data with email and password.

--- src/domain/dtos/LoginAuthDto.ts
+++ src/domain/dtos/LoginAuthDto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginAuthDto {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'The email of the user',
+  })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'SecurePassword123!',
+    description: 'The password of the user',
+  })
   password: string;
 }
