fd2999c18ddd638ef3360260887d1dcf42a519ad
Walttinho <walter.netto@live.com>
2024-07-01 05:44:47 -0300

    feat(dto): add CreateAccountDto - Introduced CreateAccountDto to define the structure for creating new user accounts.

--- src/domain/dtos/CreateAccountDto.ts
+++ src/domain/dtos/CreateAccountDto.ts
@@ -1,16 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { AccountType } from '@prisma/client';
 import { IsEnum, IsNotEmpty, IsPositive, IsString } from 'class-validator';
 
 export class CreateAccountDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'Account 01',
+    description: 'The name of the account',
+  })
   name: string;
 
   @IsNotEmpty()
   @IsEnum(AccountType)
+  @ApiProperty({ example: 'CURRENT', description: 'The type of the account' })
   type: AccountType;
 
   @IsNotEmpty()
   @IsPositive()
+  @ApiProperty({
+    example: 1000,
+    description: 'The initial balance of the account',
+  })
   balance: number;
 }
