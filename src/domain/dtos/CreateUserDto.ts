9d08a8b78d184a3b0060b89b1c0c11fdb6e721de
Walttinho <walter.netto@live.com>
2024-07-01 05:44:47 -0300

    feat(dto): add CreateUserDto - Defined CreateUserDto to encapsulate user creation data including name, email, and password.

--- src/domain/dtos/CreateUserDto.ts
+++ src/domain/dtos/CreateUserDto.ts
@@ -1,17 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'John Doe', description: 'The name of the user' })
   name: string;
 
   @IsNotEmpty()
   @IsEmail({}, { message: 'Invalid email format' })
   @Transform(({ value }) => value.toLowerCase())
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'The email of the user',
+  })
   email: string;
 
   @IsNotEmpty()
   @MinLength(8)
+  @ApiProperty({
+    example: 'SecurePassword123!',
+    description: 'The password of the user',
+  })
   password: string;
 }
